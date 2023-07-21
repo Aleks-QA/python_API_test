1228243e244682e0f81f1d25a9b9f7dd89253d8c
Alex_QA <al694000n@gmail.com>
2023-07-22 00:10:46 +0300

    added

---
+++ API_tests/test_get_one_joke_from_all_category.py
@@ -0,0 +1,20 @@
+import requests
+
+
+class TestGetJoke:
+    def test_get_one_joke_from_all_category_(self):
+        """Получить одну шутку из каждой категории"""
+
+        response = requests.get("https://api.chucknorris.io/jokes/categories")  # получить список категорий
+        list_category = response.json()
+
+        assert response.status_code == 200, "Wrong status code"
+
+        for i in list_category:  # Получить шутку из каждой категории
+            params = {"category": i}
+            response = requests.get("https://api.chucknorris.io/jokes/random", params=params)
+            print(f"Категория - {response.json()['categories']},\nШутка -  {response.json()['value']} \n")
+
+            assert response.status_code == 200, "Wrong status code"
+            assert response.json()['categories'] == [i], "Wrong joke category"
+
