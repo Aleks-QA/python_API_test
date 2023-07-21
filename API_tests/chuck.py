2fedfc6da693d908202e6c796c30fa8597339687
Alex_QA <al694000n@gmail.com>
2023-07-22 00:10:40 +0300

    added

--- API_tests/chuck.py
+++ API_tests/chuck.py
@@ -16,10 +16,19 @@ class TestNewJoke:
         response.encoding = "utf-8"  # для надежности
         print(response.json()["value"])
 
-        response_json = response.json()
-        print(response_json.get("value"))
+    def get_joke_from_sport_category(self):
+        """get_joke"""
+        data = {}
+        params = {"category": "sport"}
+        headers = {}
+        url = "https://api.chucknorris.io/jokes/random"
+        response = requests.get(url, params=params, headers=headers, data=data)
+        response.encoding = "utf-8"  # для надежности
+        print("value - ", response.json()["value"])
+        print("categories - ", response.json()["categories"])
 
 
 random_joke = TestNewJoke()
 random_joke.get_random_joke()
+random_joke.get_joke_from_sport_category()
 
