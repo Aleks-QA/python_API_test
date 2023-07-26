561928116914f4be85b18fe00b70e1021334cfb4
Alex_QA <al694000n@gmail.com>
2023-07-26 22:50:16 +0300

    update

--- tests/test_google_maps_api.py
+++ tests/test_google_maps_api.py
@@ -8,7 +8,13 @@ class TestCreatePlace:
     def test_create_new_place(self):
 
         print("\nМетод POST")
-        response_post: Response = GoogleMapsApi.create_new_place()
+        response_post = GoogleMapsApi.create_new_place()
+        check_post = response_post.json()
+        place_id = check_post["place_id"]
+        print('place_id - ', place_id)
+
+        print("\nМетод GET")
+        response_get = GoogleMapsApi.get_new_place(place_id=place_id)
 
 
 
--- utils/api.py
+++ utils/api.py
@@ -5,7 +5,7 @@ from utils.http_methods import HttpMethods
 """Методы для тестирования Google Maps API"""
 
 base_url = "https://rahulshettyacademy.com"  # Базовая URL
-key = {"?key": "qaclick123"}  # Параметр для всех запросов
+key = "?key=qaclick123"  # Параметр для всех запросов
 
 
 class GoogleMapsApi:
@@ -32,9 +32,21 @@ class GoogleMapsApi:
         post_resource = '/maps/api/place/add/json'  # ресурс метода POST
         post_url = base_url + post_resource
         print(post_url)
-        response_post = HttpMethods.post(url=post_url, body=json_for_create_new_place)
-        print(response_post.text)
-        return response_post
+        post_response = HttpMethods.post(url=post_url, body=json_for_create_new_place)
+        print(post_response.text)
+        return post_response
+
+    @staticmethod
+    def get_new_place(place_id):
+        """Метод для проверки новой локации"""
+        get_resource = '/maps/api/place/get/json'  # ресурс метода GET
+        get_url = base_url + get_resource + key + '&place_id=' + place_id
+        print(get_url)
+
+        response_get = HttpMethods.get(url=get_url)
+        print(response_get.text)
+        return response_get
+
 
 
 
