d01d57b8c7fa01699ac5bf19c54e03593eacdc77
Alex_QA <al694000n@gmail.com>
2023-07-30 12:18:14 +0300

    added Docker and requirements

---
+++ Dockerfile
@@ -0,0 +1,25 @@
+FROM python
+#WORKDIR — эта директива устанавливает текущий рабочий каталог для инструкций RUN , CMD , ENTRYPOINT , COPY и ADD .
+WORKDIR /test_project/
+#COPY — аналогично ADD но источником может быть только локальный файл или каталог.
+COPY / .
+#RUN - команды, которые будут выполняться в процессе сборки.
+RUN pip install -r requirements.txt
+#ENV - позволяет определить переменную среды.(prod or dev)
+#ENV ENV=prod
+#CMD - команда, которая будет выполняться при запуске контейнера.
+CMD python -s -m pytest --alluredir=test_results/ /test_project/tests/
+
+
+
+
+
+
+
+
+
+
+
+
+#   docker-compose up --build           # запуск
+#   allure serve test_results/          # отчет
--- tests/test_google_maps_api.py
+++ tests/test_google_maps_api.py
@@ -10,7 +10,7 @@ class TestCreatePlace:
     @allure.description("Creating, modifying and deleting a new location")
     def test_create_new_place(self):
         """Создание, изменение и удаление новой локации"""
-        print("\nМетод POST")
+        print("\nМетод POST - создание новой локации")
         response_post = GoogleMapsApi.create_new_place()
         check_post = response_post.json()
         place_id = check_post["place_id"]
@@ -20,7 +20,7 @@ class TestCreatePlace:
         Assertions.assert_json_has_keys(response_post, expected_keys)
         Assertions.assert_json_value_by_name(response_post, "status", 'OK')
 
-        print("\nМетод GET POST")
+        print("\nМетод GET POST - проверка создания новой локации")
         response_get = GoogleMapsApi.get_new_place(place_id=place_id)
         Assertions.assert_status_code(response_get, 200)
         # list_keys = list(json.loads(response_get.text))  # Получить список ключей из JSON
@@ -29,28 +29,28 @@ class TestCreatePlace:
         Assertions.assert_json_has_keys(response_get, expected_keys)
         Assertions.assert_json_value_by_name(response_get, "address", 'street')
 
-        print("\nМетод PUT")
+        print("\nМетод PUT - изменение новой локации")
         response_put = GoogleMapsApi.put_new_place(place_id=place_id, address="New York")
         Assertions.assert_status_code(response_put, 200)
         expected_keys = ["msg"]
         Assertions.assert_json_has_keys(response_put, expected_keys)
         Assertions.assert_json_value_by_name(response_put, "msg", 'Address successfully updated')
 
-        print("\nМетод GET PUT")
+        print("\nМетод GET PUT - проверка изменения новой локации")
         response_get = GoogleMapsApi.get_new_place(place_id=place_id)
         Assertions.assert_status_code(response_get, 200)
         expected_keys = ['location', 'accuracy', 'name', 'phone_number', 'address', 'types', 'website', 'language']
         Assertions.assert_json_has_keys(response_get, expected_keys)
         Assertions.assert_json_value_by_name(response_get, "address", 'New York')
 
-        print("\nМетод DELETE")
+        print("\nМетод DELETE - удаление созданной локации")
         response_delete = GoogleMapsApi.delete_new_place(place_id=place_id)
         Assertions.assert_status_code(response_delete, 200)
         expected_keys = ['status']
         Assertions.assert_json_has_keys(response_delete, expected_keys)
         Assertions.assert_json_value_by_name(response_delete, "status", 'OK')
 
-        print("\nМетод GET DELETE")
+        print("\nМетод GET DELETE - проверка удаления созданной локации")
         response_get = GoogleMapsApi.get_new_place(place_id=place_id)
         Assertions.assert_status_code(response_get, 404)
         expected_keys = ["msg"]
