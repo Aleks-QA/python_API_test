6981ae18c8acfbfe92355543aaf4f25b377cb090
Alex_QA <al694000n@gmail.com>
2023-08-11 20:05:45 +0300

    added allure

--- tests/test_star_wars_api.py
+++ tests/test_star_wars_api.py
@@ -1,32 +1,45 @@
+import allure
+from utils.assertions import Assertions
 import requests
 
 
+@allure.epic("Preserving the names of characters who starred alongside Darth Vader 'swapi.dev'")
 def test_save_character_names_star_wars():
-    """Сохранение имен всех персонажей, которые снимались в фильмах с Дарт Вейдером, в тестовый файл"""
-    get_url_darth_vader = "https://swapi.dev/api/people/4/"
-    response = requests.get(get_url_darth_vader)
-    list_films_darth_vader = response.json()['films']  # получение списка фильмов с Darth Vader
-    print("\nфильмы с Darth Vader -", list_films_darth_vader)
-    list_character = []  # ссылки на персонажей
+    """ Сохранение имен всех персонажей, которые снимались в фильмах с Дарт Вейдером, в текстовый файл """
 
-    """Получение информации о фильмах с Darth Vader и получение ссылок на персонажей этих фильмов"""
-    for i in list_films_darth_vader:
-        response_films = requests.get(i)  # получение информации о фильмах с Darth Vader
-        list_character_films = response_films.json()['characters']  # получение списка ссылок на персонажей фильма
-        print("\nсписок ссылок на персонажей фильма - ", list_character_films)
+    with allure.step(f"Getting a list of Darth Vader movies"):
+        get_url_darth_vader = "https://swapi.dev/api/people/4/"  # url с информацией о персонаже Дарт Вейдер
+        response = requests.get(get_url_darth_vader)
+        Assertions.assert_status_code(response, 200)
+        Assertions.assert_json_has_key(response, 'films')
 
-        for character_film in list_character_films:  # проверка дубликатов ссылок на персонажей
-            if character_film not in list_character:
-                list_character.append(character_film)  # добавление ссылки на персонажа
-                # print("добавлена ссылка на персонажа -", character_film)
+        list_films_darth_vader = response.json()['films']  # получение списка фильмов с Darth Vader
+        print("\nфильмы с Darth Vader -", list_films_darth_vader)
 
-    """Получение имени персонажа и запись в файл"""
-    for character in list_character:
-        response_character = requests.get(character)  # получение информации о персонаже
+    with allure.step(f"Getting references to the characters in these movies"):
+        """Получение информации о фильмах с Darth Vader и получение ссылок на персонажей этих фильмов"""
+        list_character = []  # ссылки на персонажей
+        for i in list_films_darth_vader:
+            response_films = requests.get(i)  # получение информации о фильмах с Darth Vader
+            Assertions.assert_status_code(response_films, 200)
+            Assertions.assert_json_has_key(response_films, 'characters')
 
-        name_character = response_character.json()['name']  # получение имени персонажа
+            list_character_films = response_films.json()['characters']  # получение списка ссылок на персонажей фильма
+            print("\nсписок ссылок на персонажей фильма - ", list_character_films)
 
-        file = open("name_character.txt", "a+", encoding="utf-8")  # Запись имен персонажей в файл
-        file.write(f"{name_character}\n")
-        file.close()
+            for character_film in list_character_films:  # проверка дубликатов ссылок на персонажей
+                if character_film not in list_character:
+                    list_character.append(character_film)  # добавление ссылки на персонажа
 
+    with allure.step(f"Getting a character name and writing it to a file"):
+        """Получение имени персонажа и запись в файл"""
+        for character in list_character:
+            response_character = requests.get(character)  # получение информации о персонаже
+            Assertions.assert_status_code(response_character, 200)
+            Assertions.assert_json_has_key(response_character, 'name')
+
+            name_character = response_character.json()['name']  # получение имени персонажа
+
+            file = open("name_character.txt", "a+", encoding="utf-8")  # Запись имен персонажей в файл
+            file.write(f"{name_character}\n")
+            file.close()
--- utils/api.py
+++ utils/api.py
@@ -1,3 +1,4 @@
+import allure
 from utils.http_methods import HttpMethods
 
 
@@ -10,67 +11,71 @@ class GoogleMapsApi:
     @staticmethod
     def create_new_place():
         """Метод для создания новой локации"""
-        json_for_create_new_place = {
-            "location": {
-                "lat": -38.383494,
-                "lng": 33.427362
-            }, "accuracy": 50,
-            "name": "Front house",
-            "phone_number": "(+91) 983 893 3937",
-            "address": f'street',
-            "types": [
-                "shoe park",
-                "shop"
-            ],
-            "website": "http://google.com",
-            "language": "French-IN"
-        }
+        with allure.step(f"Method for creating a new location"):
+            json_for_create_new_place = {
+                "location": {
+                    "lat": -38.383494,
+                    "lng": 33.427362
+                }, "accuracy": 50,
+                "name": "Front house",
+                "phone_number": "(+91) 983 893 3937",
+                "address": f'street',
+                "types": [
+                    "shoe park",
+                    "shop"
+                ],
+                "website": "http://google.com",
+                "language": "French-IN"
+            }
 
-        post_resource = '/maps/api/place/add/json'  # ресурс метода POST
-        post_url = GoogleMapsApi.base_url + post_resource
-        print(post_url)
-        response_post = HttpMethods.post(url=post_url, body=json_for_create_new_place)
-        print(response_post.text)
-        return response_post
+            post_resource = '/maps/api/place/add/json'  # ресурс метода POST
+            post_url = GoogleMapsApi.base_url + post_resource
+            print(post_url)
+            response_post = HttpMethods.post(url=post_url, body=json_for_create_new_place)
+            print(response_post.text)
+            return response_post
 
     @staticmethod
     def get_new_place(place_id):
-        """Метод для проверки новой локации"""
-        get_resource = '/maps/api/place/get/json'  # ресурс метода GET
-        get_url = GoogleMapsApi.base_url + get_resource + GoogleMapsApi.key + '&place_id=' + place_id
-        print(get_url)
+        """Метод для получения локации"""
+        with allure.step(f"Method for get a location"):
+            get_resource = '/maps/api/place/get/json'  # ресурс метода GET
+            get_url = GoogleMapsApi.base_url + get_resource + GoogleMapsApi.key + '&place_id=' + place_id
+            print(get_url)
 
-        response_get = HttpMethods.get(url=get_url)
-        print(response_get.text)
-        return response_get
+            response_get = HttpMethods.get(url=get_url)
+            print(response_get.text)
+            return response_get
 
     @staticmethod
     def put_new_place(place_id, address):
-        """Метод для изменения новой локации"""
-        put_resource = '/maps/api/place/update/json'  # ресурс метода PUT
-        put_url = GoogleMapsApi.base_url + put_resource + GoogleMapsApi.key + '&place_id=' + place_id
-        print(put_url)
+        """Метод для изменения локации"""
+        with allure.step(f"Method for changing the location"):
+            put_resource = '/maps/api/place/update/json'  # ресурс метода PUT
+            put_url = GoogleMapsApi.base_url + put_resource + GoogleMapsApi.key + '&place_id=' + place_id
+            print(put_url)
 
-        json_for_update_new_location = {
-            "place_id": place_id,
-            "address": address,
-            "key": "qaclick123"
-        }
-        response_put = HttpMethods.put(put_url, json_for_update_new_location)
-        print(response_put.text)
-        return response_put
+            json_for_update_new_location = {
+                "place_id": place_id,
+                "address": address,
+                "key": "qaclick123"
+            }
+            response_put = HttpMethods.put(put_url, json_for_update_new_location)
+            print(response_put.text)
+            return response_put
 
     @staticmethod
     def delete_new_place(place_id):
-        """Метод для удаления новой локации"""
-        delete_resource = '/maps/api/place/delete/json'  # ресурс метода DELETE
-        delete_url = GoogleMapsApi.base_url + delete_resource + GoogleMapsApi.key
-        print(delete_url)
+        """Метод для удаления локации"""
+        with allure.step(f"Method for deleting a location"):
+            delete_resource = '/maps/api/place/delete/json'  # ресурс метода DELETE
+            delete_url = GoogleMapsApi.base_url + delete_resource + GoogleMapsApi.key
+            print(delete_url)
 
-        json_for_delete_new_location = {"place_id": place_id}
-        response_put = HttpMethods.put(delete_url, json_for_delete_new_location)
-        print(response_put.text)
-        return response_put
+            json_for_delete_new_location = {"place_id": place_id}
+            response_put = HttpMethods.put(delete_url, json_for_delete_new_location)
+            print(response_put.text)
+            return response_put
 
 
 
